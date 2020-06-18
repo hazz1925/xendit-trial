3b9dcb69dcf135d88b51188f25d252ebc1fa95ff
Hazeem Salim <hazeem.salim@iflix.com>
2020-06-18 14:39:41 +0800

    Restucture

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
 
 // entities
 import { Notification } from './entities/notification.entity'
